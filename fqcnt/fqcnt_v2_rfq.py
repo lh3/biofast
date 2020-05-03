247b6dc441140e04b1ede9a6caabfa344a571361
Heng Li <lh3@me.com>
2020-05-03 17:47:59 -0400

    simplified rfq

--- fqcnt/fqcnt_v2_rfq.py
+++ fqcnt/fqcnt_v2_rfq.py
@@ -6,26 +6,25 @@ def readfq(fp): # this is a generator function
 		if not last: # the first record or a record following a fastq
 			for l in fp: # search for the start of the next record
 				if l[0] in '>@': # fasta/q header line
-					last = l[:-1] # save this line
+					last = l.rstrip() # save this line
 					break
 		if not last: break
-		name, seqs, last = last[1:].partition(" ")[0], [], None
+		name, seq, last = last[1:].partition(" ")[0], "", None
 		for l in fp: # read the sequence
 			if l[0] in '@+>':
-				last = l[:-1]
+				last = l.rstrip()
 				break
-			seqs.append(l[:-1])
+			seq += l.rstrip()
 		if not last or last[0] != '+': # this is a fasta record
-			yield name, ''.join(seqs), None # yield a fasta record
+			yield name, seq, None # yield a fasta record
 			if not last: break
 		else: # this is a fastq record
-			seq, leng, seqs = ''.join(seqs), 0, []
+			qual = ""
 			for l in fp: # read the quality
-				seqs.append(l[:-1])
-				leng += len(l) - 1
-				if leng >= len(seq): # have read enough quality
+				qual += l.rstrip()
+				if len(qual) >= len(seq): # have read enough quality
 					last = None
-					yield name, seq, ''.join(seqs); # yield a fastq record
+					yield name, seq, qual; # yield a fastq record
 					break
 			if last: # reach EOF before reading enough quality
 				yield name, seq, None # yield a fasta record instead
