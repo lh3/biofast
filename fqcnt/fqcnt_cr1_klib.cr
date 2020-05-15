ff60f2d35829e920f7e884a98b5064301c8fc261
Heng Li <lh3@me.com>
2020-05-14 23:23:50 -0400

    fastq parsing; this version is slow...

---
+++ fqcnt/fqcnt_cr1_klib.cr
@@ -0,0 +1,17 @@
+require "../lib/klib"
+
+include Klib
+
+if ARGV.size < 1
+	puts "Usage: fqcnt <in.fq>"
+	exit(0)
+end
+
+fp = Bufio(GzipReader).new(ARGV[0])
+n, slen, qlen = 0, 0, 0
+each_fastx(fp) do |hdr, seq, qual|
+	n += 1
+	slen += seq.size
+	qlen += qual == nil ? 0 : qual.not_nil!.size
+end
+puts "#{n}\t#{slen}\t#{qlen}"
